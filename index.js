4675d0fb755e456c0d783386add0590c1d5d0931
Oscar Calderon <oscar@ualett.com>
2021-08-03 18:01:38 -0400

    create

--- index.js
+++ index.js
@@ -13,36 +13,73 @@ odoo.connect(function (err) {
     return console.log(err);
   }
   console.log("Connected to Odoo server.");
+  // let inParams = [];
+  // inParams.push([[1, "=", 1]]);
+  // let params = [];
+  // params.push(inParams);
+
+  // // 1- Login
+  // odoo.connect(function (err) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   console.log("Connected to Odoo server.");
+  // });
+
+  // // 4- Read
+  // odoo.execute_kw("account.move", "search", params, function (err, value) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   console.log(value);
+  //   var inParams = [];
+  //   inParams.push(value); //ids
+  //   inParams.push([]);
+  //   var params = [];
+  //   params.push(inParams);
+
+  //   odoo.execute_kw("account.move", "read", params, function (err2, value2) {
+  //     if (err2) {
+  //       return console.log(err2);
+  //     }
+  //     console.log("Result: ", value2);
+  //   });
+  // });
   let inParams = [];
-  inParams.push([[1, "=", 1]]);
+  inParams.push({
+    name: "Soy yo Oscar",
+    payment_state: "not_paid",
+    move_type: "out_invoice",
+    invoice_date: "2021-08-15",
+    //invoice_date_due: "2021-08-15",
+    invoice_payment_term_id: 11,
+    // amount_untaxed: 500,
+    // amount_tax: 500 * 0.24,
+    partner_id: 10,
+  });
   let params = [];
   params.push(inParams);
-
-  // 1- Login
-  odoo.connect(function (err) {
+  odoo.execute_kw("account.move", "create", params, function (err, value) {
     if (err) {
       return console.log(err);
     }
-    console.log("Connected to Odoo server.");
-  });
-
-  // 4- Read
-  odoo.execute_kw("account.move", "search", params, function (err, value) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(value);
-    var inParams = [];
-    inParams.push(value); //ids
-    inParams.push([]);
-    var params = [];
-    params.push(inParams);
-
-    odoo.execute_kw("account.move", "read", params, function (err2, value2) {
-      if (err2) {
-        return console.log(err2);
-      }
-      console.log("Result: ", value2);
-    });
+    console.log("Result: ", value);
   });
 });
+
+// odoo.connect(function (err) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   var inParams = [];
+//   inParams.push([3, 4, 5]); //id to delete
+//   var params = [];
+//   params.push(inParams);
+//   odoo.execute_kw("account.move", "unlink", params, function (err, value) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("Result: ", value);
+//   });
+// });
